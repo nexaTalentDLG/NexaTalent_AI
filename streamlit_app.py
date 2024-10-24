eb4f962828f3c0d3bbba05caccad972c43c94180
nexaTalentDLG <ryandlg@nexatalent.com>
2024-10-24 11:59:02 +0000

    git commit -m "Add Google Apps Script to import data into spreadsheet"

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,80 @@
+import openai
 import streamlit as st
+import time
+import os
+import dotenv
+dotenv.load_dotenv()
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+assistant_id = "asst_6fbevnHGincxZ4n5eOFd4lu9"
+
+client = openai
+
+if "start_chat" not in st.session_state:
+    st.session_state.start_chat = False
+if "thread_id" not in st.session_state:
+    st.session_state.thread_id = None
+
+st.set_page_config(page_title="NT.ai", page_icon=":robot:")
+
+if st.sidebar.button("Start Chat"):
+    st.session_state.start_chat = True
+    thread = client.beta.threads.create()
+    st.session_state.thread_id = thread.id
+
+st.title("NexaTalent AI Assitant")
+st.write("Developing content to support hiring teams.")
+
+if st.sidebar.button("Exit Chat"):
+    st.session_state.messages = []  # Clear the chat history
+    st.session_state.start_chat = False  # Reset the chat state
+    st.session_state.thread_id = None
+
+if st.session_state.start_chat:
+    if "openai_model" not in st.session_state:
+        st.session_state.openai_model = "gpt-4o"
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Input content"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        client.beta.threads.messages.create(
+                thread_id=st.session_state.thread_id,
+                role="user",
+                content=prompt
+            )
+        
+        run = client.beta.threads.runs.create(
+            thread_id=st.session_state.thread_id,
+            assistant_id=assistant_id,
+            instructions="You are an Assistant that can help interviewers create unique situational interview questions with follow up questions based on provided interview competencies, information provided, and NexaTalent Pillars of Excellence. You can also evaluate candidate responses or offer sample responses based on NexaTalent grading parameters for each, concern, mild-concern, mixed, mild-strength, or strength. You will ask the interviewer what task they need help with, what job and level are being interviewed, how many interviews will be conducted and any specific competencies, Pillar(s) of Excellence or technical skills each interviewer will be focusing on during their session. If nothing is provided, you will utilize the information provided to create a thorough interview structure. "
+        )
+
+        while run.status != 'completed':
+            time.sleep(1)
+            run = client.beta.threads.runs.retrieve(
+                thread_id=st.session_state.thread_id,
+                run_id=run.id
+            )
+        messages = client.beta.threads.messages.list(
+            thread_id=st.session_state.thread_id
+        )
+
+        # Process and display assistant messages
+        assistant_messages_for_run = [
+            message for message in messages 
+            if message.run_id == run.id and message.role == "assistant"
+        ]
+        for message in assistant_messages_for_run:
+            st.session_state.messages.append({"role": "assistant", "content": message.content[0].text.value})
+            with st.chat_message("assistant"):
+                st.markdown(message.content[0].text.value)
+
+else:
+    st.write("Click 'Start Chat' to begin.")
